527d7b4aa13d000e2a2151bf9a73f975c545da1d
Daniel Monti <dmonti@users.noreply.github.com>
2025-02-09 11:23:33 -0300

    Spring Boot start project

---
+++ src/main/kotlin/com/dmonti/JwtExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.dmonti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtExampleApplication>(*args)
+}
